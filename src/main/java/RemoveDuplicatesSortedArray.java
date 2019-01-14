324c0844ac89b3db37e2ab21f1296a7ddbcaa21d
anuj jain <anuj.aj07@gmail.com>
2019-01-15 00:26:15 +0530

    first program

---
+++ src/main/java/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicatesSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int j=0;
+        for(int i=1 ;i<nums.length;i++) {
+            if(nums[i]!=nums[j]) {
+                j++;
+                nums[j]=nums[i];
+            }
+        }
+        return j+1;
+    }
+}
